ae0ed156c42a219b107abcadf719435bc529271a
your name <long.cv.61cntt@ntu.edu.vn>
2022-04-06 18:59:49 +0700

    6-4

--- 61133899_TH6-4/src/Client_01.java
+++ 61133899_TH6-4/src/Client_01.java
@@ -1,4 +1,35 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
 
-public class Client_01 {
+public class Client_01{
+
+    public static void main(String[] args) throws IOException{
+        Socket mySocket = null;
+        DataOutputStream dos = null;
+        DataInputStream dis=null;
+        Scanner sc = new Scanner(System.in);
+
+        try {
+
+            mySocket = new Socket("localhost", 8000);
+            dos = new DataOutputStream(mySocket.getOutputStream());
+            dis = new DataInputStream(mySocket.getInputStream());
+            Scanner input = new Scanner(System.in);
+            String s = null;
+           
+        }
+        catch(Exception e) {
+            System.out.print("Ngat ket noi");
+            dis.close();
+            dos.close();
+            mySocket.close();
+            e.printStackTrace();
+
+        }
+
+    }
 
 }
--- 61133899_TH6-4/src/Server_01.java
+++ 61133899_TH6-4/src/Server_01.java
@@ -1,4 +1,24 @@
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Server_01 {
+	private static ServerSocket serverSocket = null;
+	public static void main(String[] args) throws IOException{
+
+		
+		try {
+	        serverSocket = new ServerSocket(8000);
+	        System.out.print("Server ss\n" );
+	       
+	    }
+	    catch(Exception e) {
+	    	
+	        serverSocket.close();
+
+	        System.out.print(e.getMessage());
+
+	    }
+	}
 
 }
