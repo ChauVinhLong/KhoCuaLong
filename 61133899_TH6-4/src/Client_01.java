e3fc7fba30d6ec24e72b8d43f97d4cf02a184c55
your name <long.cv.61cntt@ntu.edu.vn>
2022-04-06 19:12:18 +0700

    6-4

--- 61133899_TH6-4/src/Client_01.java
+++ 61133899_TH6-4/src/Client_01.java
@@ -10,16 +10,31 @@ public class Client_01{
         Socket mySocket = null;
         DataOutputStream dos = null;
         DataInputStream dis=null;
-        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(System.in);
 
         try {
 
-            mySocket = new Socket("localhost", 8000);
+            mySocket = new Socket("localhost", 8888);
             dos = new DataOutputStream(mySocket.getOutputStream());
             dis = new DataInputStream(mySocket.getInputStream());
             Scanner input = new Scanner(System.in);
             String s = null;
-           
+            while(true)
+            {
+                System.out.print("\nClient: ");
+                s =input.nextLine();
+                dos.writeUTF(s);
+               
+                String str=dis.readUTF();
+                System.out.print("Server : "+str);
+                
+                //System.out.println("\nNhập 0 để kết thúc phiên");
+				//String rep = sc.next();
+				//if (rep.equalsIgnoreCase("0")) {
+					//System.out.println("---Phiên kết thúc---");
+					//break;
+				//}
+            }
         }
         catch(Exception e) {
             System.out.print("Ngat ket noi");
--- 61133899_TH6-4/src/Server_01.java
+++ 61133899_TH6-4/src/Server_01.java
@@ -1,24 +1,78 @@
-
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Server_01 {
-	private static ServerSocket serverSocket = null;
-	public static void main(String[] args) throws IOException{
 
-		
-		try {
-	        serverSocket = new ServerSocket(8000);
-	        System.out.print("Server ss\n" );
-	       
-	    }
-	    catch(Exception e) {
-	    	
-	        serverSocket.close();
+private static ServerSocket serverSocket = null;
+public static void main(String[] args) throws IOException{
 
-	        System.out.print(e.getMessage());
+    DataOutputStream dos = null;
+    DataInputStream dis=null;
+    try {
+        serverSocket = new ServerSocket(8888);
+        System.out.print("Server ss\n" );
+        Socket clientSocket = null;
+        clientSocket = serverSocket.accept();
+        dos=new DataOutputStream(clientSocket.getOutputStream());
+        dis=new DataInputStream(clientSocket.getInputStream());
+        String inline="";
 
-	    }
-	}
+        while(true)
+        {
+            inline = dis.readUTF();
+            char ch[]=inline.toCharArray();
+            if(Character.isDigit(ch[0]))
+                {
+                int i=Integer.parseInt(inline);
+                Scanner sc = new Scanner(System.in);
+                String rep = sc.next();
+                if (rep.equalsIgnoreCase("0")) {
+					System.out.println("---Phiên kết thúc---");
+					break;
+				}
+                switch(i)
+                {
+                    
+                    case 1:inline="1.txt";
+                    break;
+                    case 2:inline="2.txt";
+                    break;
+                    case 3:inline="3.txt";
+                    break;
+                    case 4:inline="4.txt";
+                    break;
+                    case 5:inline="5.txt";
+                    break;
+                    case 6:inline="6.txt";
+                    break;
+                    case 7:inline="7.txt";
+                    break;
+                    case 8:inline="8.txt";
+                    break;
+                    case 9:inline="9.txt";
+                    break;
+                    case 10:inline="10.txt";
+                    break;  
+                }
+                dos.writeUTF(inline);
+            }
+            else
+                dos.writeUTF("Nhập từ 1 - 10");
+        }
+
+    }
+    catch(Exception e) {
+        dos.close();
+        serverSocket.close();
+        dis.close();
+        System.out.print(e.getMessage());
+
+    }
 
 }
+
+} 
